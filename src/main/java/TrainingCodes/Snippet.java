9969a09f1fa41940d15e5836abe837924610dc75
varsh <varsh@192.168.1.50>
2023-03-20 10:21:38 +0530

    old Programs Added

---
+++ src/main/java/TrainingCodes/Snippet.java
@@ -0,0 +1,8 @@
+package TrainingCodes;
+
+public class Snippet {
+	public static void main(String[] args) {
+		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
+	}
+}
+
